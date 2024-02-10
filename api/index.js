205ab297e00419cd6dd7642679a18c436fe4cd6d
Sharath S Rao <sharathsrao2@gmail.com>
2024-02-10 23:08:44 +0530

    Create user model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose  from 'mongoose'
 
+dotenv.config()
+
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(()=> {
+        console.log('DB connected successfully')
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
 const app = express()
 
 app.listen(3000, () => {
-    console.log("Server running succ on 3000")
+    console.log("Server running succ on 3000!")
 })
