5160907e36727f9cfda61e3fac2bb1bc37128980
Sharath S Rao <sharathsrao2@gmail.com>
2024-02-10 22:44:33 +0530

    included api directory

--- homer/src/App.jsx
+++ homer/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
+import Header from './component/Header'
 
 const App = () => {
   return (<BrowserRouter>
+  <Header/>
   <Routes>
     <Route path='/' element={<Home/>}></Route>
     <Route path='/about' element={<About/>}></Route>
