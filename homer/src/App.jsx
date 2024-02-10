e3e1475730f6ea28dde6f091ded9ca23e8979639
Sharath S Rao <sharathsrao2@gmail.com>
2024-02-10 13:21:41 +0530

    created pages and routes

--- homer/src/App.jsx
+++ homer/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+
 const App = () => {
-  return (
-    <div>App</div>
+  return (<BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}></Route>
+    <Route path='/about' element={<About/>}></Route>
+    <Route path='/profile' element={<Profile/>}></Route>
+    <Route path='/signIn' element={<SignIn/>}></Route>
+  </Routes>
+  </BrowserRouter>
   )
 }
 
